39fa799c8c4049f08b95c4db311f62b367b3f6f3
Sebastian <110711768+Cybiii@users.noreply.github.com>
2024-01-03 07:05:08 -0800

    User model

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,32 @@
 import express from "express";
-import http from 'http'
-import bodyParser from 'body-parser'
-import cookieParser from 'cookie-parser'
-import compression from 'compression'
-import cors from 'cors'
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose, { mongo } from "mongoose";
 
-const app = express()
+const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     credentials: true,
-}))
+  })
+);
 
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 server.listen(8080, () => {
-    console.log('Server running on http://localhost:8080')
-})
+  console.log("Server running on http://localhost:8080");
+});
+
+const MONGO_URL =
+  "mongodb+srv://sebastian:<R55zLd3KDBN4oERn>@cluster0.jhoduti.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => console.log(error));
