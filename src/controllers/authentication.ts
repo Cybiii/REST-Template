1d05b48bdc286a2afd0dadf6adf3737ea1c9533f
Sebastian <110711768+Cybiii@users.noreply.github.com>
2024-01-03 09:54:12 -0800

    Delete and update controllers

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -10,7 +10,9 @@ export const login = async (req: express.Request, res: express.Response) => {
       return res.sendStatus(400);
     }
 
-    const user = await getUserByEmail(email).select('+authentication.salt +authentication.password')
+    const user = await getUserByEmail(email).select(
+      "+authentication.salt +authentication.password"
+    );
 
     if (!user) {
       return res.sendStatus(400);
@@ -22,10 +24,21 @@ export const login = async (req: express.Request, res: express.Response) => {
       return res.sendStatus(403);
     }
 
+    const salt = random();
+    user.authentication.sessionToken = authentication(
+      salt,
+      user._id.toString()
+    );
 
+    await user.save();
 
+    res.cookie("SEBASTIAN-AUTH", user.authentication.sessionToken, {
+      domain: "localhost",
+      path: "/",
+    });
 
-
+    return res.status(200).json(user).end();
+    
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
